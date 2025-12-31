16729e037854107f7eb47c847879490608af9dab
Anael Ben Shabat <anaelb24@cs.colman.ac.il>
2025-12-31 17:11:01 +0200

    feat: Add Constants file and static student image for initial project scaffolding

---
+++ app/src/main/java/com/ofirandanael/studentapp/Constants.kt
@@ -0,0 +1,8 @@
+package com.ofirandanael.studentapp
+
+// Application-wide intent extras and constants for the Students App
+object Constants {
+    const val EXTRA_STUDENT_ID = "com.ofirandanael.studentapp.EXTRA_STUDENT_ID"
+    const val EXTRA_STUDENT_INDEX = "com.ofirandanael.studentapp.EXTRA_STUDENT_INDEX"
+}
+
